837df4cc7d2ada9cc302b4cd00792d10b0c67bbc
fernando <sucrezhang@qq.com>
2017-02-26 23:02:25 +0800

    runloop practice

---
+++ RunLoopTest/RunLoopTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunLoopTest
+//
+//  Created by fernando on 2017/2/26.
+//  Copyright © 2017年 fernando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunLoopTest/RunLoopTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunLoopTest
+//
+//  Created by fernando on 2017/2/26.
+//  Copyright © 2017年 fernando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
